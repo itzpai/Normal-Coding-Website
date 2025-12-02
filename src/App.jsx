a893a1d636253fd686af9759874d432404160d25
Thiha Aung <92858542+thihathiha@users.noreply.github.com>
2025-12-02 17:27:38 +0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,32 @@ import Projects from "./pages/Projects";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/learning-path" element={<LearningPath/>} />
-        <Route path="/feedback" element={<Feedback/>} />
-        <Route path="/courses" element={<Courses/>} />
-        <Route path="/courses/uiux" element={<Uiux/>} />
-        <Route path="/courses/web-foundation" element={<WebFoundation/>} />
-        <Route path="/courses/styling-mastery" element={<StylingMastery/>} />
-        <Route path="/courses/react-mastery" element={<ReactMastery/>} />
-        <Route path="/aboutus" element={<AboutUs/>} />
-        <Route path="/projects" element={<Projects/>} />
-        <Route path="*" element={<ErrorPage/>} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
+        <div className="flex flex-col min-h-screen justify-between">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/learning-path" element={<LearningPath />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/courses/uiux" element={<Uiux />} />
+            <Route path="/courses/web-foundation" element={<WebFoundation />} />
+            <Route
+              path="/courses/styling-mastery"
+              element={<StylingMastery />}
+            />
+            <Route path="/courses/react-mastery" element={<ReactMastery />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
